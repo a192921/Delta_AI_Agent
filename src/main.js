e3866047f99c4fbcbc87e95e3afd364d619eda28
吳卿瑤 <queena@queenadeMacBook-Pro-2.local>
2026-02-09 20:47:39 +0800

    20260209_v2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
+import axios from 'axios';
 
 const app = createApp(App)
 app.use(Antd).mount('#app')
+// 設定基礎路徑
+axios.defaults.baseURL = 'http://your-api-server.com:8080'; 
+
+// 設定超時時間（選填）
+axios.defaults.timeout = 10000;
+
+// 設定預設 Header（選填）
+axios.defaults.headers.post['Content-Type'] = 'application/json'
--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,21 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
+
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     proxy: {
+//       // 當你的請求路徑以 /api 開頭時，會自動轉發
+//       '/api': {
+//         target: 'http://your-backend-api-server.com', // 真實後端位置
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '') // 視後端路徑決定是否移除 /api 前綴
+//       }
+//     }
+//   }
+// })
